3f6718f6deee8d9ee6b8fb305c41d05bca86f2f9
kai <oneillkai@icloud.com>
2020-02-23 14:19:31 -0800

    load all cards to db

--- models/card.js
+++ models/card.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import random from "mongoose-simple-random";
+import tarot_data from "./tarot_info.json"
 
 
 const cardSchema = new mongoose.Schema({
@@ -67,21 +68,13 @@ const Card = mongoose.model("Card", cardSchema);
 
 
 const saveCards = () => {
-  let cardHash = {
-    "The Magician": "https://www.elliotoracle.com/blog/2018/1/30/the-card-of-the-day-the-magician",
-    "The High Priestess": "https://www.elliotoracle.com/blog/2018/10/2/the-card-of-the-day-the-high-priestess",
-    "The Empress": "https://www.elliotoracle.com/blog/2017/7/10/the-card-of-the-day-the-empress"
-  };
 
-  Object.keys(cardHash).forEach(function (name) {
-    let newCard = new Card({ 
-      name: name,
-      link: cardHash[name]
-    });
+  tarot_data.cards.forEach(function (card) {
+    let newCard = new Card(card);
 
     newCard.save(function(err, newCard) {
       if (err) return console.error(err);
-      console.log(`${name} has been saved`);
+      console.log(`${newCard.name} has been saved`);
     });
   });
 }
