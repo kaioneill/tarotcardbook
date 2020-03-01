539bedef2349b3aa6f13a0ec0f62b5be49f860d4
kai <oneillkai@icloud.com>
2020-03-01 18:49:59 -0500

    view all cards separated by suit

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -8,10 +8,25 @@ export class List extends Component {
     this.state = {
       cards: []
     };
-    fetch("/cards/list")
+    this.getSuit = this.getSuit.bind(this);
+    this.toggleDepressed = this.toggleDepressed.bind(this);
+    this.getSuit(null, 'Trump');
+  }
+
+  getSuit = (e, suit) => {
+    this.setState({cards: []});
+    fetch("/cards/suit?suit=" + suit)
       .then(res => res.json())
       .then(cards => this.setState({ cards }))
+      .then(this.toggleDepressed(e))
       .catch(e => console.log(e));
+  }
+
+  toggleDepressed = (e) => {
+    if (e) {
+      document.querySelector(".depressed").classList.remove("depressed");
+      e.target.classList.add("depressed");
+    }
   }
 
   render() {
@@ -19,6 +34,11 @@ export class List extends Component {
       return (
         <div className="List">
           <h2>all the things</h2>
+          <button className="depressed" onClick={(e) => this.getSuit(e, 'Trump')}>major arcana</button>
+          <button className="" onClick={(e) => this.getSuit(e, 'Cups')}>cups</button>
+          <button className="" onClick={(e) => this.getSuit(e, 'Swords')}>swords</button>
+          <button className="" onClick={(e) => this.getSuit(e, 'Pentacles')}>pentacles</button>
+          <button className="" onClick={(e) => this.getSuit(e, 'Wands')}>wands</button>
           <div className="card-container flex flex-center flex-wrap">
             loading...
           </div>
@@ -28,6 +48,11 @@ export class List extends Component {
       return (
         <div className="List">
           <h2>all the things</h2>
+          <button className="depressed" onClick={(e) => this.getSuit(e, 'Trump')}>major arcana</button>
+          <button className="" onClick={(e) => this.getSuit(e, 'Cups')}>cups</button>
+          <button className="" onClick={(e) => this.getSuit(e, 'Swords')}>swords</button>
+          <button className="" onClick={(e) => this.getSuit(e, 'Pentacles')}>pentacles</button>
+          <button className="" onClick={(e) => this.getSuit(e, 'Wands')}>wands</button>
           <div className="card-container flex flex-center flex-wrap">
             {this.state.cards.map(card => (
               <Card card={card} key={card.name} noReverse={true} />
--- routes/cards.js
+++ routes/cards.js
@@ -4,7 +4,19 @@ var router = express.Router();
 
 router.get('/list', function (req, res, next) {
 
-  Card.find().exec(function (err, cards) {
+  Card.find().sort([['img']]).exec(function (err, cards) {
+    if (err) return console.error(err);
+    console.log(cards);
+    res.send(cards);
+  });
+
+});
+
+router.get('/suit', function (req, res, next) {
+
+  Card.find({ suit: req.query.suit }).sort([
+    ['number']
+  ]).exec(function (err, cards) {
     if (err) return console.error(err);
     console.log(cards);
     res.send(cards);
