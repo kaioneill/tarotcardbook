3f771afa9bb18c58927284280aaf151121b9f988
Kai O'Neill <oneillkai@icloud.com>
2020-04-04 19:45:40 -0700

    home cooked dropdown in the works

--- client/src/components/BuildCard.jsx
+++ client/src/components/BuildCard.jsx
@@ -9,32 +9,70 @@ class BuildCard extends Component {
       query: this.props.card ? this.props.card.name : "",
       results: [],
       card: this.props.card || {},
-      reversed: this.props.reversed || false
+      reversed: this.props.reversed || false,
     };
     this.updateResults = this.updateResults.bind(this);
+    this.moveDropdown = this.moveDropdown.bind(this);
+    this.cycleDropdown = this.cycleDropdown.bind(this);
     this.checkMatch = this.checkMatch.bind(this);
     this.setCard = this.setCard.bind(this);
     this.setReversed = this.setReversed.bind(this);
   }
 
+  moveDropdown = (event) => {
+    if (
+      event.target.parentNode.querySelector(
+        `#results${this.props.index} .dropdown-option`
+      ) &&
+      event.keyCode === 40
+    ) {
+      event.preventDefault();
+      event.target.parentNode
+        .querySelector(`#results${this.props.index} .dropdown-option`)
+        .focus();
+    }
+  };
+
+  cycleDropdown = (event) => {
+    if (event.keyCode === 38) {
+      if (event.target.previousSibling) {
+        event.preventDefault();
+        event.target.previousSibling.focus();
+      } else {
+        event.target.parentNode.previousSibling.previousSibling.focus();
+      }
+    } else if (event.keyCode === 40 && event.target.nextSibling) {
+      event.preventDefault();
+      event.target.nextSibling.focus();
+    }
+  };
+
   updateResults = (event) => {
     let timeout = null;
     if (event) this.setState({ query: event.target.value });
-    this.checkMatch(event.target, event.target.value);
+    // this.checkMatch(event.target, event.target.value);
     clearTimeout(timeout);
     timeout = setTimeout(() => {
       fetch("cards/search?query=" + this.state.query)
-        .then(res => res.json())
-        .then(results => this.setState({ results: results }))
-        .then(_ => (this.state.query === '' ? this.setState({ results: [] }) : null))
-        .catch(e => console.log(e));
+        .then((res) => res.json())
+        .then((results) => this.setState({ results: results }))
+        .then((_) =>
+          this.state.query === "" ? this.setState({ results: [] }) : null
+        )
+        .catch((e) => console.log(e));
     }, 500);
   };
 
   checkMatch = (element, value) => {
-    let datalist = element.parentNode.querySelector(`#results${this.props.index}`);
+    let datalist = element.parentNode.querySelector(
+      `#results${this.props.index}`
+    );
     if (!datalist.options.length) return;
-    if(Array.from(datalist.options).filter(option => option.value.toLowerCase() === value.toLowerCase()).length) {
+    if (
+      Array.from(datalist.options).filter(
+        (option) => option.value.toLowerCase() === value.toLowerCase()
+      ).length
+    ) {
       this.setCard(value);
     } else {
       this.setState({ card: {} });
@@ -43,24 +81,28 @@ class BuildCard extends Component {
   };
 
   setCard = (value) => {
-    let matches = this.state.results.filter(card => card.name === value.replace(" Reversed", ""));
+    let matches = this.state.results.filter(
+      (card) => card.name === value.replace(" Reversed", "")
+    );
     if (matches.length) {
       this.setState({ card: matches[0] });
       this.props.addCard(matches[0], this.props.index, this.state.reversed);
     }
   };
 
-
   setReversed = (event) => {
     if (this.state.card.name) {
       this.setState({ reversed: event.target.checked }, () => {
-        this.props.addCard(this.state.card, this.props.index, this.state.reversed);
+        this.props.addCard(
+          this.state.card,
+          this.props.index,
+          this.state.reversed
+        );
       });
     } else {
       this.setState({ reversed: event.target.checked });
     }
-  }
-
+  };
 
   render() {
     return (
@@ -69,24 +111,32 @@ class BuildCard extends Component {
         <div className="flex flex-center vertical">
           <div>
             <input
-              list={`results${this.props.index}`}
+              // list={`results${this.props.index}`}
               placeholder="search for a card"
               type="text"
               value={this.state.query}
-              onChange={event => this.updateResults(event)}
+              onChange={(event) => this.updateResults(event)}
+              onKeyUp={(event) => this.moveDropdown(event)}
             ></input>
-            <datalist id={`results${this.props.index}`}>
-              {this.state.results.map(card => (
-                <option
+            <br />
+            <div id={`results${this.props.index}`} className="flex vertical">
+              {this.state.results.map((card) => (
+                <input
+                  type="text"
+                  className="dropdown-option"
                   key={card.name}
                   value={card.name + (this.state.reversed ? " Reversed" : "")}
-                />
+                  onChange={() => {}}
+                  onKeyUp={(event) => this.cycleDropdown(event)}
+                >
+                  {/* {card.name + (this.state.reversed ? " Reversed" : "")} */}
+                </input>
               ))}
-            </datalist>
-            <br/>
+            </div>
+            <br />
             <input
               type="checkbox"
-              onChange={event => this.setReversed(event)}
+              onChange={(event) => this.setReversed(event)}
               checked={this.state.reversed}
             ></input>
             reversed
